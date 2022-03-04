c47bf8a7fca569beb1493eb9acd66b595051883c
Hleb Haiduk <hleb.haiduk@ulta.com>
2022-03-04 13:11:42 +0100

    Implement project resetup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import Button, { ButtonContext } from './components/Button/Button';
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <div>
+        <Button
+          textLabel={'Secondary Test'}
+          dataTest={'Secondary'}
+          onClick={() => console.log('Click')}
+        />
+      </div>
+      <div style={{ marginTop: '20px' }}>
+        <Button
+          buttonContext={ButtonContext.PRIMARY}
+          textLabel={'Primary Test'}
+          dataTest={'primary'}
+          onClick={() => console.log('Click')}
+        />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,5 @@
+import { ReactComponent as DualRingLoader } from './dual-ring.loader.svg';
+
+const Loader = () => <DualRingLoader />;
+
+export default Loader;
